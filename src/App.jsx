85eb2b365bc7f41d5cc298f8c3c3b55b190d25a1
LAPTOP-JGJ72FAB\sidas <sidasierra24@gmail.com>
2023-05-02 19:42:01 -0400

    fork exec wait

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Code from "./structures/Code.class"
 import Line from "./structures/Line.class"
 import ProcessView from "./components/ProcessView"
 import { useState } from "react"
+import callingFork from "./blocks/callingFork"
 
 const App = () => {
   const [view, setView] = useState("process")
@@ -10,26 +11,7 @@ const App = () => {
   if(view === "process") {
     return(
       <div>
-        <ProcessView codeBlocks={[
-          new Code([
-            new Line(
-              "printf('1', getRandomNumber())",
-              0,
-              () => console.log('1', Math.random())
-            ),
-            new Line(
-              "printf('2')",
-              0,
-              () => console.log('2', Math.random())
-            ),
-            new Line(
-              "printf('3')",
-              0,
-              () => console.log('3', Math.random())
-            )
-          ],
-          "Code Block Description Here...")
-        ]} />
+        <ProcessView codeBlocks={[callingFork]} />
         <button onClick={() => setView("view")}>Change View!</button>
       </div>
     )
